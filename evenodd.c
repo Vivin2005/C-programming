e1897cf3a008aa7814b3689e4afc1fe9249ff375
Vivin <139694166+Vivin2005@users.noreply.github.com>
2023-07-17 12:59:57 +0530

    Even or Odd
    Even or odd number

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter an interger\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("IT IS EVEN NUMBER");
+	}
+	else
+	{
+		printf("IT IS ODD NUMBER");
+	}
+}
